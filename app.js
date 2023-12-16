8f685474a5ef74d8baf403241e598b7697340ab0
Ilya Kozionov <ik.ilyakozionov@yandex.ru>
2023-12-16 23:48:09 +0300

    feat: TASK-6 Освоение WebSocket

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const Fastify = require('fastify');
-
-const fastify = Fastify({
+const fastify = require('fastify')({
   logger: true
 });
+const { redis } = require('./services/redis');
 
+fastify.register(require('@fastify/websocket'));
 fastify.register(require('./routes/root'));
 
 const start = async () => {
   try {
+    await redis.connect();
     await fastify.listen({ port: 3000, host: '0.0.0.0' });
   } catch (err) {
     fastify.log.error(err);
--- routes/root.js
+++ routes/root.js
@@ -1,6 +1,25 @@
+const setToRedis = require('../routesLogic/setToRedis');
+const getFromRedis = require('../routesLogic/getFromRedis');
+const { set, get } = require('../services/redis');
+
 async function routes(fastify) {
-  fastify.get('/intro', async () => ({ where: 'intro' }));
-  fastify.get('/', async () => ({ hello: 'world' }));
+  fastify.get('/socket', { websocket: true }, async (connection) => {
+    connection.socket.on('message', async (message) => {
+      let response;
+      const redisData = await get(message);
+      if (redisData) {
+        console.log('from redis');
+        response = redisData;
+      } else {
+        console.log('from ML');
+        response = Math.random() * 100;
+        await set(message, response);
+      }
+      connection.socket.send(response);
+    });
+  });
+  fastify.get('/', getFromRedis);
+  fastify.post('/', setToRedis);
 }
 
 module.exports = routes;
